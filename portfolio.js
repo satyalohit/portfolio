45154987bd67378961f62c78db1c2f577389a1ee
satyalohit <167320919+satyalohit@users.noreply.github.com>
2021-06-10 11:22:45 +0530

    commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+const sr=ScrollReveal({
+	origin:'left',distance:'80px',duration:'2000',reset:true})
+
+sr.reveal(".image",{ delay: 500 })
+
+sr.reveal('.about-image',{delay:600})
+sr.reveal('.about-info',{delay:800})
+sr.reveal('.imagelast',{delay:500,origin:'top'})
+sr.reveal('.text',{delay:500})
+sr.reveal('.skilllistitems',{interval:500,delay:500})
+sr.reveal('.solo',{interval:500,delay:500})
